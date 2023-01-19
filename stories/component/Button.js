7241d758bf79dd647ab345bb37c89bac6d238b8c
zun5761 <zun5761@gmail.com>
2023-01-19 09:48:49 +0900

    button disabled 추가

--- stories/component/Button.js
+++ stories/component/Button.js
@@ -7,6 +7,7 @@ export const createButton = ({
   btnColor,
   size,
   label,
+  disabled = false,
   onClick,
 }) => {
   let btn;
@@ -14,9 +15,11 @@ export const createButton = ({
     btn = document.createElement('a');
     btn.href = '#';
     btn.role = 'button';
+    if(disabled) btn.ariaDisabled = true;
   }else{
     btn = document.createElement('button');
     btn.type = 'button';
+    if(disabled) btn.disabled = true;
   }
   btn.innerText = label;
   btn.addEventListener('click', function(e){
@@ -29,6 +32,7 @@ export const createButton = ({
   if(size && size !== 'defalut') classAray.push(size);
   if(line) classAray.push('line');
   if(round) classAray.push('round');
+  if(disabled && tag === 'a') classAray.push('disabled');
   btn.className = classAray.join(' ');
 
   return btn;
